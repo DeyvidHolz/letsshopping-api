60f03fbd97305eed5e3e2d10800e7f751d50dd66
= <deyvidholzt@gmail.com>
2021-04-03 05:30:56 -0300

    feat: adds jest, axios and dotenv

---
+++ src/test/user.test.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const URL = process.env.URL + (process.env.PORT ? ':' + process.env.PORT : '');
+
+test('Should authenticate user', (done) => {
+  axios.post(`${URL}/api/auth`, { username: 'deyvidholz', password: '123456' })
+    .then(res => {
+      expect(res.status).toBe(200)
+      done()
+    })
+})
