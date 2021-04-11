3b5db635f4a8f52f608a67adf18854d74265958d
= <deyvidholzt@gmail.com>
2021-04-11 17:40:36 -0300

    feat: add update and delete user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,18 +1,14 @@
 import express, { Request, Response } from "express";
-import UserController from "../controllers/user.controller";
 
+import UserController from "../controllers/user.controller";
 import AuthMiddleware from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
 router.get("/all", AuthMiddleware, UserController.getAll);
-
 router.get("/", AuthMiddleware, UserController.get);
-
 router.post("/", UserController.create);
-
-router.put("/", AuthMiddleware, (req: Request, res: Response) => {});
-
-router.delete("/", (req: Request, res: Response) => {});
+router.put("/", AuthMiddleware, UserController.update);
+router.delete("/", UserController.delete);
 
 export default router;
