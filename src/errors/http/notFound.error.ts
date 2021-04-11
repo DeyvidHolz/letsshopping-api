31352d4d3d5f36a8dfa04411ce54b0260a651c4b
= <deyvidholzt@gmail.com>
2021-04-11 14:39:49 -0300

    feat: create notFound error

---
+++ src/errors/http/notFound.error.ts
@@ -0,0 +1,9 @@
+import { HTTPError, httpErrorData } from "./error";
+
+class NotFound extends HTTPError {}
+
+function notFound(data: httpErrorData): NotFound {
+  return new NotFound(data.message, data.errors).setStatus(404);
+}
+
+export default notFound;
