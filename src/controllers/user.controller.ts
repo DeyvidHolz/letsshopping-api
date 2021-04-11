030f3c9cc85fac098c21a36a0413a505fc27ce3f
= <deyvidholzt@gmail.com>
2021-04-11 17:40:09 -0300

    feat: add new methods

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -12,6 +12,7 @@ import StringHelper from "../helpers/string.helper";
 import UserValidator from "../validators/user.validator";
 import unprocessableEntity from "../errors/http/unprocessableEntity.error";
 import internalServerError from "../errors/http/internalServer.error";
+import notFound from "../errors/http/notFound.error";
 
 class UserController {
   public static create = async (req, res) => {
@@ -112,6 +113,84 @@ class UserController {
       message: "Invalid username or password.",
     }).send(res);
   };
+
+  public static update = async (req, res) => {
+    const userRepository = await getConnection().getRepository(User);
+
+    const userIDisEmpty = req.body.id === undefined || req.body.id === "";
+
+    if (userIDisEmpty) {
+      return unprocessableEntity({
+        message: "Field 'id' is required.",
+      }).send(res);
+    }
+
+    const user = await userRepository.findOne(Number(req.body.id));
+
+    if (!user) {
+      return notFound({
+        message: `User with ID ${req.body.id} not found.`,
+      }).send(res);
+    }
+
+    const currentPasswordIsEmpty =
+      req.body.currentPassword === undefined || req.body.currentPassword === "";
+
+    if (currentPasswordIsEmpty) {
+      return unprocessableEntity({
+        message: "Field 'currentPassword' is required.",
+      }).send(res);
+    }
+
+    if (!CryptHelper.checkPassword(req.body.currentPassword, user.password)) {
+      return unprocessableEntity({
+        message: "Password incorrect.",
+      }).send(res);
+    }
+
+    const data = {
+      password: req.body.password ?? user.password,
+      firstName: req.body.firstName ?? user.firstName,
+      lastName: req.body.lastName ?? user.lastName,
+      email: req.body.email ?? user.email,
+      birthDate: req.body.birthDate ?? user.birthDate,
+    };
+
+    user.firstName = StringHelper.uppercaseFirst(data.firstName);
+    user.lastName = StringHelper.uppercaseFirst(data.lastName);
+    user.password = CryptHelper.encryptPassword(data.password);
+    user.email = data.email;
+    user.birthDate = data.birthDate;
+
+    const validation = new UserValidator(user);
+
+    if (validation.hasErrors()) {
+      return unprocessableEntity({
+        message: "Invalid data.",
+        errors: validation.validationErrors,
+      }).send(res);
+    }
+
+    try {
+      await userRepository.save(user);
+
+      if (user.id) {
+        delete user.password;
+        return res.status(201).json({ message: "User created", user });
+      }
+    } catch (err) {
+      if (err.code === "23505") {
+        return unprocessableEntity({
+          message: "This username is already in use.",
+          errors: validation.validationErrors,
+        }).send(res);
+      }
+
+      return internalServerError({ message: err.message }).send(res);
+    }
+  };
+
+  public static delete = async (req, res) => {};
 }
 
 export default UserController;
