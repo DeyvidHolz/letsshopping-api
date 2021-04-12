f2e725bfb0023a1f27417999b54bb8fcd99cf471
= <deyvidholzt@gmail.com>
2021-04-12 00:51:44 -0300

    feat: user default messages

---
+++ src/messages/user.messages.ts
@@ -0,0 +1,24 @@
+import { messages } from '../types/messages/message.types';
+
+const userMessages: messages = {
+  created: 'User created.',
+  updated: 'User updated.',
+  deleted: 'User deleted.',
+  invalidData: 'Invalid data.',
+  invalidEmail: 'Invalid email.',
+  invalidCredentials: 'Invalid username or password.',
+  invalidPassword: 'Password incorrect.',
+  alreadyExists: 'This username is already in use.',
+  alreadyDeleted: 'Invalid token or user already deleted.',
+
+  searchByEmailNotFound: {
+    msg: 'User with email {email} not found.',
+    replace: [{ from: 'email', to: 'email' }],
+  },
+  searchByIDNotFound: {
+    msg: 'User with ID {id} not found.',
+    replace: [{ from: 'id', to: 'id' }],
+  },
+};
+
+export default userMessages;
