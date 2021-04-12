755cd4b930d1757e735123d1e5fe267f97445f28
= <deyvidholzt@gmail.com>
2021-04-12 00:51:16 -0300

    refactor: return set to default return standard

--- src/helpers/messages.ts
+++ src/helpers/messages.ts
@@ -1,15 +1,17 @@
 import { message } from '../types/messages/message.types';
 
-export default function (message: message, object?: object): string {
+const getMessage = (message: message, object?: object): string => {
   if (typeof message !== 'object') return message;
 
   let replacedMessage: string = message.msg;
 
   message.replace.forEach((replace) => {
     replace.to = replace.to.replace(/\{(.+)\}/, '$1');
-    const regex = new RegExp(`\\{${replace.to}\\}`, 'g');
-    replacedMessage = replacedMessage.replace(regex, object[replace.from]);
+    const regex = new RegExp(`\\{${replace.from}\\}`, 'g');
+    replacedMessage = replacedMessage.replace(regex, object[replace.to]);
   });
 
   return replacedMessage;
-}
+};
+
+export { getMessage };
