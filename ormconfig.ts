316322686f4770bf5bdff030d40f4cd85d1b343e
Deyvid Holz <deyvidholzt@gmail.com>
2021-05-02 06:02:55 -0300

    feat(docker): add environment file

---
+++ ormconfig.ts
@@ -0,0 +1,18 @@
+export default {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: true,
+  logging: false,
+  entities: ['src/entities/**/*.ts'],
+  migrations: ['src/migrations/**/*.ts'],
+  subscribers: ['src/subscribers/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entities',
+    migrationsDir: 'src/migrations',
+    subscribersDir: 'src/subscribers',
+  },
+};
