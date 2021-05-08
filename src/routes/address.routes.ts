70ae3ac856d1fbe0d28917f9ecf9d859ac8448ab
Deyvid Holz <deyvidholzt@gmail.com>
2021-05-07 21:34:10 -0300

    refactor(address.routes): implement address validator middleware

--- src/routes/address.routes.ts
+++ src/routes/address.routes.ts
@@ -2,13 +2,26 @@ import express from 'express';
 
 import AddressController from '../controllers/address.controller';
 import AuthMiddleware from '../middlewares/auth.middleware';
+import AddressValidatorMiddleware from '../middlewares/validators/addressValidator.middleware';
 
 const router = express.Router();
 
 router.get('/all', AuthMiddleware, AddressController.getAll);
 router.delete('/:id', AddressController.delete);
 router.get('/:id', AuthMiddleware, AddressController.get);
-router.post('/', AuthMiddleware, AddressController.create);
-router.patch('/:id', AuthMiddleware, AddressController.update);
+
+router.post(
+  '/',
+  AuthMiddleware,
+  AddressValidatorMiddleware.create,
+  AddressController.create,
+);
+
+router.patch(
+  '/:id',
+  AuthMiddleware,
+  AddressValidatorMiddleware.update,
+  AddressController.update,
+);
 
 export default router;
