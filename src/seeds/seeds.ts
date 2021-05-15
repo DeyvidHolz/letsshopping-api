f552be560d6646ec5b5dc4c35273755dbcd85bcf
Deyvid Holz <deyvidholzt@gmail.com>
2021-05-15 00:18:23 -0300

    feat(seeds): add config RUN_SEEDS on .env file

--- src/seeds/seeds.ts
+++ src/seeds/seeds.ts
@@ -1,9 +1,15 @@
 import 'colors';
+import dotenv from 'dotenv';
 
 // import { createCategorySeeds } from './category.seeds';
 
+dotenv.config();
+
 export async function createSeeds() {
-  console.log('Creating seeds...'.blue);
-  // createCategorySeeds();
-  console.log('All seeds created'.blue);
+  const runSeeds = process.env.RUN_SEEDS === 'true';
+  if (runSeeds) {
+    console.log('Creating seeds...'.blue);
+    // createCategorySeeds();
+    console.log('All seeds created'.blue);
+  }
 }
