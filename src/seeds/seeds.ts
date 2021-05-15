21f0f6eda619871995b4644749f8c3dad4d903a1
Deyvid Holz <deyvidholzt@gmail.com>
2021-05-15 00:11:08 -0300

    feat: add seeds function

---
+++ src/seeds/seeds.ts
@@ -0,0 +1,9 @@
+import 'colors';
+
+// import { createCategorySeeds } from './category.seeds';
+
+export async function createSeeds() {
+  console.log('Creating seeds...'.blue);
+  // createCategorySeeds();
+  console.log('All seeds created'.blue);
+}
